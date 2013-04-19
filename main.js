bf6f17665b71e24e3d297721961759f8928d6f4b
David Su <david.d.su@gmail.com>
2013-04-19 18:12:04 -0400

    inti

---
+++ main.js
@@ -0,0 +1,32 @@
+﻿
+$(document).ready(function () {
+	var curText = "";
+	var curStats;
+	//curText = "asdf";
+	main();
+});
+
+function main() {
+	/**text**/
+	$("#poetry").bind("input propertychange", function() {
+		curText = $("#poetry").val();
+		curStats = textstatistics(curText);
+		console.log(curText);
+		console.log(curStats);
+		console.log(analyze(curText));
+	});
+	
+	/**audio**/
+	var synth = T("saw");
+	$("#play").click(function() {
+		synth.play();
+	});
+	$("#pause").click(function() {
+		synth.pause();
+	});
+}
+
+function getStats() {
+	var s = "";
+	
+}
